4b92d39e62cdfd52f0355f4994c63fd26dbd1017
borysmatveichuk <boris.matveychuck@gmail.com>
2021-08-25 00:24:58 +0300

    Structure for saving data

---
+++ src/ZentserSensor.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+
+/*!
+ *  @brief  Class that stores state for interacting with Zentser system
+ *  @param  id
+ *          sensor's id has given by the server. Required.
+ *  @param  name
+ *          sensor's name. Not required, used only for pretty logging
+ *  @param  float
+ * 			    value returned by a sensor. Required
+ */
+struct Sensor {
+
+  Sensor(String id, String name = "Sensor") {
+    this->id = id;
+    this->name = name;
+  }
+
+  String id;
+  String name;
+  float value;
+};
